4a40e96a2cbe9c6c77a42fde77171c7a445fa850
Alex Bihas <alex.bigas.2004@gmail.com>
2023-07-09 23:58:41 +0300

    Added loader and fetching effect handler

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
-import { tempMovieData } from './data';
-import { tempWatchedData } from './data';
+// import { tempMovieData } from './data';
+// import { tempWatchedData } from './data';
 
 import { Main } from './Main';
 import { MovieList } from './MovieList';
@@ -11,11 +11,34 @@ import { Search } from './Search';
 import { WatchedSummary } from './WatchedSummary';
 import { WatchedList } from './WatchedList';
 import { Box } from './Box';
+import { Loader } from './Loader';
+
+const API_KEY = 'https://www.omdbapi.com/?i=tt3896198&apikey=45871cbf';
 
 export default function App() {
-  const [movies, setMovies] = useState(tempMovieData);
-  const [watched, setWatched] = useState(tempWatchedData);
+  const [movies, setMovies] = useState([]);
+  const [watched, setWatched] = useState([]);
+  const [query, setQuery] = useState('interstellar');
+  const [isLoading, setIsLoading] = useState(false);
   const moviesCount = movies.length;
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      setIsLoading(true);
+      const res = await fetch(`${API_KEY}&s=${query}`);
+      const resData = await res.json();
+
+      setMovies(resData.Search);
+      setIsLoading(false);
+    };
+    // fetch(`${API_KEY}&s=interstellar`)
+    //   .then(res => res.json())
+    //   .then(res => {
+    //     setMovies(res.Search);
+    //   });
+
+    fetchMovies();
+  }, []);
 
   return (
     <React.Fragment>
@@ -24,9 +47,7 @@ export default function App() {
         <NumResults moviesCount={moviesCount} />
       </NavBar>
       <Main>
-        <Box>
-          <MovieList movies={movies} />
-        </Box>
+        <Box>{isLoading ? <Loader /> : <MovieList movies={movies} />}</Box>
         <Box>
           <WatchedSummary watched={watched} />
           <WatchedList watched={watched} />
